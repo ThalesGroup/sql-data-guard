c9604a6a7b9c8bf73849482c687a3173498a16fa
orin <orin@imperva.com>
2025-03-04 09:44:30 +0200

    remove boto usage

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,10 +1,13 @@
+import datetime
+import hashlib
+import hmac
+import http
 import json
 import logging
 import os
+from http.client import HTTPSConnection
 from pathlib import Path
 from typing import Optional, List
-
-import boto3
 
 _DEFAULT_MODEL_ID = "anthropic.claude-instant-v1"
 _PROJECT_FOLDER = Path(os.path.dirname(os.path.abspath(__file__))).parent.absolute()
@@ -14,11 +17,14 @@ def get_project_folder() -> str:
     return str(_PROJECT_FOLDER)
 
 
-def init_creds_from_file():
-    file_name = os.path.join(get_project_folder(), "aws", "config")
-    if os.path.exists(file_name):
-        os.environ["AWS_CONFIG_FILE"] = file_name
-        logging.info("AWS config file set")
+def init_env_from_file():
+    full_file_name = os.path.join(get_project_folder(), "config", "aws.env.list")
+    if os.path.exists(full_file_name):
+        logging.info(f"Going to set env variables from file: {full_file_name}")
+        with open(full_file_name) as f:
+            for line in f:
+                key, value = line.strip().split("=")
+                os.environ[key] = value
 
 def get_model_ids() -> List[str]:
     return ["anthropic.claude-instant-v1", "anthropic.claude-v2:1", "anthropic.claude-v3"]
@@ -26,18 +32,70 @@ def get_model_ids() -> List[str]:
 
 def invoke_llm(system_prompt: Optional[None], user_prompt: str, model_id: str = _DEFAULT_MODEL_ID) -> str:
     logging.info(f"Going to invoke LLM. Model ID: {model_id}")
-    bedrock_client = boto3.client(service_name='bedrock-runtime')
-    response = bedrock_client.invoke_model(
-        body=_format_model_body(user_prompt, system_prompt, model_id),
-        modelId=model_id,
-    )
-    response_json = json.loads(response.get("body").read())
+    prompt = _format_model_body(user_prompt, system_prompt, model_id)
+    response_json = _invoke_bedrock_model(prompt, model_id)
     response_text = _get_response_content(response_json, model_id)
     logging.info(f"Got response from LLM. Response length: {len(response_text)}")
     return response_text
 
 
-def _format_model_body(prompt: str, system_prompt: Optional[str], model_id: str) -> str:
+
+def _invoke_bedrock_model(prompt_body: dict, model_id: str) -> dict:
+    region = os.environ['AWS_DEFAULT_REGION']
+    access_key = os.environ['AWS_ACCESS_KEY_ID']
+    secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
+
+    host = f"bedrock-runtime.{region}.amazonaws.com"
+
+    t = datetime.datetime.now(datetime.UTC)
+    amz_date = t.strftime('%Y%m%dT%H%M%SZ')
+    date_stamp = t.strftime('%Y%m%d')
+
+    json_payload = json.dumps(prompt_body)
+
+    hashed_payload = hashlib.sha256(json_payload.encode()).hexdigest()
+
+    canonical_uri = f"/model/{model_id}/invoke"
+    canonical_querystring = ""
+    canonical_headers = f"host:{host}\nx-amz-date:{amz_date}\n"
+    signed_headers = "host;x-amz-date"
+    canonical_request = (
+        f"POST\n{canonical_uri}\n{canonical_querystring}\n"
+        f"{canonical_headers}\n{signed_headers}\n{hashed_payload}"
+    )
+
+    algorithm = "AWS4-HMAC-SHA256"
+    credential_scope = f"{date_stamp}/{region}/bedrock/aws4_request"
+    string_to_sign = (
+        f"{algorithm}\n{amz_date}\n{credential_scope}\n"
+        f"{hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()}"
+    )
+
+    def sign(key, msg):
+        return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
+
+    k_date = sign(("AWS4" + secret_key).encode("utf-8"), date_stamp)
+    k_service = sign(sign(k_date, region), "bedrock")
+    signature = hmac.new(sign(k_service, "aws4_request"),
+                         string_to_sign.encode("utf-8"), hashlib.sha256).hexdigest()
+
+    headers = {
+        "Content-Type": "application/json",
+        "X-Amz-Bedrock-Model-Id": model_id,
+        "x-amz-date": amz_date,
+        "Authorization": f"{algorithm} Credential={access_key}/{credential_scope}, "
+            f"SignedHeaders={signed_headers}, Signature={signature}"
+    }
+
+    conn = http.client.HTTPSConnection(host)
+    try:
+        conn.request("POST", canonical_uri, body=json_payload, headers=headers)
+        return json.loads(conn.getresponse().read())
+    finally:
+        conn.close()
+
+
+def _format_model_body(prompt: str, system_prompt: Optional[str], model_id: str) -> dict:
     if system_prompt is None:
         system_prompt = "You are a SQL generator helper"
     if "claude" in model_id:
@@ -69,7 +127,7 @@ def _format_model_body(prompt: str, system_prompt: Optional[str], model_id: str)
         }
     else:
         raise ValueError(f"Unknown model_id: {model_id}")
-    return json.dumps(body)
+    return body
 
 
 def _get_response_content(response_json: dict, model_id: str) -> str:
